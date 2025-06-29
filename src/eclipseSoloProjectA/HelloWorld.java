e25e147d3bca9999b73e5096306ed785d8d6ff88
rpiza12 <rpiza@emich.edu>
2025-06-29 15:57:00 -0400

    section b, step 4a

--- src/eclipseSoloProjectA/HelloWorld.java
+++ src/eclipseSoloProjectA/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Wrold!");
-
+		System.out.println("Added output for lab: Github and Eclipse Solo.");
 	}
 
 }
