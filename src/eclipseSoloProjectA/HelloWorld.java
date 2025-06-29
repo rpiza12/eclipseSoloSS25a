5b34f9f9a5c584348f1bba49b131495009e17e33
rpiza12 <rpiza@emich.edu>
2025-06-29 17:33:34 -0400

    first programmer changes for step 7.1

--- src/eclipseSoloProjectA/HelloWorld.java
+++ src/eclipseSoloProjectA/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Wrold!");
 		System.out.println("Added output for lab: Github and Eclipse Solo.");
-		System.out.println("Programmer 2: this line is added for section D, step 6.1.");
+		System.out.println("Programmer 2: (Programmer 1: step 7.1) this line is added for section D, step 6.1.");
 	}
 
 }
